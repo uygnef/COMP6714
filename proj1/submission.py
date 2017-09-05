d15491f538fb39531db1529bd4788fc22c15e6fb
uygnef <97508691@qq.com>
2017-09-05 12:01:10 +1000

    init, add proj1

---
+++ proj1/submission.py
@@ -0,0 +1,38 @@
+## Submission.py for COMP6714-Project1
+
+###################################################################################################################
+## Question No. 0:
+def add(a, b): # do not change the heading of the function
+    return a + b
+
+
+
+
+###################################################################################################################
+## Question No. 1:
+
+def gallop_to(a, val):# do not change the heading of the function
+    pass # **replace** this line with your code
+
+
+###################################################################################################################
+## Question No. 2:
+
+def Logarithmic_merge(index, cut_off, buffer_size): # do not change the heading of the function
+    pass # **replace** this line with your code
+
+
+
+
+
+###################################################################################################################
+## Question No. 3:
+
+def decode_gamma(inputs):# do not change the heading of the function
+    pass # **replace** this line with your code
+
+def decode_delta(inputs):# do not change the heading of the function
+    pass # **replace** this line with your code
+
+def decode_rice(inputs, b):# do not change the heading of the function
+    pass # **replace** this line with your code
